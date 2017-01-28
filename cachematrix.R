1e8bc4e702397ec6228e199e2da5504c7df418e7
bhawanab <meetbhawana@gmail.com>
2017-01-28 21:40:38 +0530

    My code for this assignment updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##R Assignment- Week 3- Caching the Inverse of a Matrix
+##This function creates a list with the set and get functions for both input matrix x and its inverse matrix Inv.
+makeCacheMatrix<-function(x=matrix())  #Initialized the makeCacheMatrix() with x parameter
+  {
+  n<-nrow(x)                           ##n is no. of row of x. Inv should have similar dim as x and since x should be a square matrix, nrow=ncol
+ Inv<-matrix(data=NA,nrow=n,ncol=n)    ##NA value initialized for the inverted matrix
+ set<-function(y=matrix(),n) 
+ {
+   x<<-y
+   Inv<<-matrix(data=NA,nrow=n,ncol=n)}
+ get<-function() x
+setinverse<-function(inv_matrix) Inv<<- inv_matrix
+getinverse<-function() Inv
+list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##This function calculates inverse of the input matrix. If object exists for the same value of x, the cached value is displayed.
+cacheSolve<-function(x){
+  Inv<-x$getinverse()                 
+  if (!is.na(Inv[1]))                   ##Checking only 1st value of the matrix for NA, since if one value is NA all will be NA, as initialized
+  {print("Getting cached data")
+  return(Inv)}
+  M<-x$get()
+  Inv<-solve(M)
+  x$setinverse(Inv)
+  Inv
 }
